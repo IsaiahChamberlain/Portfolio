4e27ddddaa4e69eb6936569b823eae3d258174b9
IsaiahChamberlain <110074126+IsaiahChamberlain@users.noreply.github.com>
2022-09-27 20:43:30 -0600

    Version 6

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
+const MyDate = new Date();
+var hours = MyDate.getTime();
+
 function askName(){
     
     var firstName = prompt("What is your name?");
-    alert("Hello " + firstName + ", thank you for visiting my site!" );
-    console.log(firstName);
+    if(hours >= 5 && hours <= 12){
+        alert("Goodmorning," + firstName + "welcome to my site!")
+    } else if (hours > 12 && hours <= 18) {
+        alert("Good Afternoon," + firstName + "welcome to my site!")
+    } else if (hours > 18) {
+        alert(`Good evening ${firstName} Welcome to my site`)
+    }
 }
